842cbeecd556128a68404da54793e92054e89666
Rajip2 <122016689+Rajip2@users.noreply.github.com>
2023-02-08 16:28:19 +0700

    Delete MVC directory

--- MVC/app/views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-	<h1 class="mt-3">About Me</h1>
-	<img src="<?= BASEURL; ?>/img/mobil.jpg" alt="Rajip M" width="200" class="rounded-circle shadow">
-	<p>Hallo, nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
-</div>
